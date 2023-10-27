d9d07cd4a9091d28618e018b5b6096df1c66dd5a
龙龙 <51738561+08183080@users.noreply.github.com>
2023-10-27 22:44:18 +0800

    daily

--- leetcode/L1465.py
+++ leetcode/L1465.py
@@ -1,8 +1,42 @@
 class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: list[int], verticalCuts: list[int]) -> int:
+        """
+        绝对暴力, 无脑惨胜
+        """
+        mod = 1000000007
         horizontalCuts.sort()
         verticalCuts.sort()
+        hh = []
+        vv = []
+        hn = len(horizontalCuts)
+        vn = len(verticalCuts)
+        hh.append(horizontalCuts[0])
+        for i in range(1, hn):
+            hh.append(horizontalCuts[i] - horizontalCuts[i - 1])
+        hh.append(h - horizontalCuts[hn - 1])        
+        print(hh)
+
+
+        vv.append(verticalCuts[0])
+        for i in range(1, vn):
+            vv.append(verticalCuts[i] - verticalCuts[ i - 1])
+        vv.append(w - verticalCuts[vn - 1])
+        print(vv)
+
+        # ans = 0
+        amax = max(hh)
+        bmax = max(vv)
+        print(amax)
+        print(bmax) 
         ans = 0
-        h = []
-        v = []
-        
+        ans = int((amax * bmax) % mod)  # 太大的数太大的数 int()
+        return ans
+    
+if __name__ == "__main__":
+    h = 1000000000
+    w = 1000000000
+    horizontalCuts = [2]
+    verticalCuts = [2]
+    s = Solution()
+    ans = s.maxArea(h, w, horizontalCuts, verticalCuts)  # 预期结果 81
+    print(ans)
