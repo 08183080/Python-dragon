ff7aa7a87a1e4a48541b1114ec846b55075488be
龙龙 <08183080@cumt.edu.cn>
2024-01-02 00:07:49 +0800

    优酷爬爬爬

---
+++ projects/spiders/re_demo.py
@@ -0,0 +1,21 @@
+import re
+"""
+优酷视频
+匹配相关明星的正则表达式不是很好设计
+"""
+def read_file(path):
+    with open(path, "r", encoding="utf-8") as f:
+        text = f.read()
+    # print(text)
+    return text
+
+path = "text.txt"
+text = read_file(path)
+# print(text)
+
+pattern = r'"subtitle":\s*"[^"]*导演[^"]*"[^"]*"title":\s*"([^"]+)"'
+m = re.search(pattern, text)
+print(m)
+if m:
+    print(m.group(1))
+
--- projects/spiders/youku_video.py
+++ projects/spiders/youku_video.py
@@ -1,9 +1,9 @@
 import re
 import time
 import requests
-from lxml import etree
 
-one_url = "https://v.youku.com/v_show/id_XNjAyODY3MzYxNg==.html"
+url = "https://www.youku.com/category/data?session=%7B%22subIndex%22%3A48%2C%22trackInfo%22%3A%7B%22parentdrawerid%22%3A%2234441%22%7D%2C%22spmA%22%3A%22a2h05%22%2C%22level%22%3A2%2C%22spmC%22%3A%22drawer3%22%2C%22spmB%22%3A%228165803_SHAIXUAN_ALL%22%2C%22index%22%3A1%2C%22pageName%22%3A%22page_channelmain_SHAIXUAN_ALL%22%2C%22scene%22%3A%22search_component_paging%22%2C%22scmB%22%3A%22manual%22%2C%22path%22%3A%22EP967584%22%2C%22scmA%22%3A%2220140719%22%2C%22scmC%22%3A%2234441%22%2C%22from%22%3A%22SHAIXUAN%22%2C%22id%22%3A227939%2C%22category%22%3A%22%E5%8A%A8%E6%BC%AB%22%7D&params=%7B%22type%22%3A%22%E5%8A%A8%E6%BC%AB%22%7D&pageNo=3"
+one_url = "https://v.youku.com/v_show/id_XNTk4MDM2MTc4NA==.html?spm=a2hja.12701310.filter.4212&s=dcaed107ed44453089fe"
 
 """
 UA伪装json模板
@@ -35,14 +35,15 @@ def get_conetent(url):
     intro = "" # 地区+时间+类型
     desc = "" # 简介
     actors = "" # 演员
-    directors = "" # 导演
+    director = "" # 导演
+    roles = "" # 相关明星
     heat  = "" # 热度
 
     title_pattern = r'"videoTitle":\s*"([^"]+)"'
     intro_pattern = r'"introSubTitle":\s*"([^"]+)"'
     desc_pattern = r'"desc":\s*"([^"]+)"'
     heat_pattern = r'"heat"\s*:\s*(\d+)'
-    directors_pattern = r'"subtitle":\s*"导演"(.*?)"title":\s*"([^"]+)"'  # wait
+    directors_pattern = r'"title":\s*"([^"]+)"' # GG
 
     fuck_words = "<script>sessionStorage.x5referer = window.location.href;window.location.replace" 
 
@@ -52,38 +53,61 @@ def get_conetent(url):
         
         if fuck_words in response:
             print("进入验证码校验界面了, fuck...") 
-            time.sleep(1)
+            # time.sleep(1)
 
         m1 = re.search(title_pattern, response)
         if m1:
             title = m1.group(1)
-            print(title)
+            # print(title)
         
         m2 = re.search(intro_pattern, response)
         if m2:
             intro = m2.group(1)
-            print(intro)
+            # print(intro)
 
         m3 = re.search(desc_pattern, response)
         if m3:
             desc = m3.group(1)
-            print(desc) 
+            # print(desc) 
         
         m4 = re.search(heat_pattern, response)
         if m4:
             heat = m4.group(1)
-            print(heat)
+            # print(heat)
 
-        m5 = re.search(directors_pattern, response)
-        if m5:
-            director = m5.group(1)
-            print(director)
-        
+        roles = re.findall(directors_pattern, response)
+        roles = roles[2:14] # some data noise
+        # print(roles)
+        # if m5:
+        #     director = m5.group()
+        #     print(director)
     except Exception as e:
         print(f"处理{url}失败~")
     
-    return title, intro, desc, heat
+    return title, intro, desc, heat, roles
 
-get_conetent(one_url)
+def get_all_links(url):
+    payload = {
+        "session": {"subIndex":48,"trackInfo":{"parentdrawerid":"34441"},"spmA":"a2h05","level":2,"spmC":"drawer3","spmB":"8165803_SHAIXUAN_ALL","index":1,"pageName":"page_channelmain_SHAIXUAN_ALL","scene":"search_component_paging","scmB":"manual","path":"EP400885","scmA":"20140719","scmC":"34441","from":"SHAIXUAN","id":227939,"category":"电影"},
+        "params": {"type":"电影"},
+        "pageNo": "1"
+    }
+    url = ""
+    urls = []
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
 
+        videos = data['data']['filterData']['listData']
+        for video in videos:
+            url = "https:" + video['videoLink']
+            print(url)
+            urls.append(url)
+    except Exception as e:
+        print(f"处理{url}获取link时候出现问题...")
+    return urls
+    
 
+if __name__ == "__main__":
+    print(get_conetent(one_url))
+    # get_all_links(url)
