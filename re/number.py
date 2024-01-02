d93b54f07f14597209dc0eb7d801354e6feb5e69
龙龙 <08183080@cumt.edu.cn>
2024-01-02 19:35:37 +0800

    number re

---
+++ re/number.py
@@ -0,0 +1,18 @@
+import re
+
+def pattern_match(pattern, string):
+    m = re.search(pattern, string)
+    if m:
+        print(m.group())
+
+text = r'num1 = 1234567.89 \r\n num2 = 12'
+
+integer_pattern = r'\d+'
+float_pattern = r'\d+.\d+'
+
+pattern_match(float_pattern, text)
+pattern_match(integer_pattern, text)
+
+
+
+
