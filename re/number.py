d418cc59a644014f0ef4a3a0c2a15825f1757299
龙龙 <08183080@cumt.edu.cn>
2024-01-02 19:38:10 +0800

    re。。。

--- re/number.py
+++ re/number.py
@@ -5,14 +5,10 @@ def pattern_match(pattern, string):
     if m:
         print(m.group())
 
-text = r'num1 = 1234567.89 \r\n num2 = 12'
+text = r'num1 = 34567.89 \r\n num2 = 12'
 
 integer_pattern = r'\d+'
 float_pattern = r'\d+.\d+'
 
 pattern_match(float_pattern, text)
 pattern_match(integer_pattern, text)
-
-
-
-
