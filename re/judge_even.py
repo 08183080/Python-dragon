7542d9ce2fdbcdd67f737af98ddb0d330bbcbe9e
dragon <51738561+08183080@users.noreply.github.com>
2024-01-04 22:25:46 +0800

    even number pattern

---
+++ re/judge_even.py
@@ -0,0 +1,10 @@
+import re
+"""
+test online: https://regexr.com/
+"""
+text = r'2 4 68 13 45 19 xhl dragon 100'
+even_pattern = r'\b\d*[02468]\b'
+
+match = re.findall(even_pattern, text)
+if match:
+    print(match)
