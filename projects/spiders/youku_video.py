059a8a92cca5851cbd109f4020acd139d8920fcd
龙龙 <08183080@cumt.edu.cn>
2024-01-08 23:19:43 +0800

    优酷获取comment还是需要研究研究的

--- projects/spiders/youku_video.py
+++ projects/spiders/youku_video.py
@@ -5,6 +5,8 @@ import pandas as pd
 from youku_get_links import get_all_links
 
 one_url = "https://v.youku.com/v_show/id_XNDI0NzkwNTgyNA==.html?spm=a2hja.12701310.filter.2594&s=07b8722657364fa2a485&s=07b8722657364fa2a485"
+comment_url = "https://acs.youku.com/h5/mtop.alidme.xtop.tv.order.status.get/1.0"
+url = 'https://acs.youku.com/h5/mtop.youku.columbus.ycp.query/1.0/?jsv=2.6.1&appKey=24679788&t=1704726300934&sign=ce1278e095528c2db6d8333eca4e6b84&api=mtop.youku.columbus.ycp.query&type=originaljson&v=1.0&ecode=1&dataType=json&data=%7B%22ms_codes%22%3A%222019061000%22%2C%22system_info%22%3A%22%7B%7D%22%2C%22params%22%3A%22%7B%5C%22bizKey%5C%22%3A%5C%22ycp%5C%22%2C%5C%22pageSize%5C%22%3A10%2C%5C%22time%5C%22%3A1704726300934%2C%5C%22app%5C%22%3A%5C%225200-C2cNqy93%5C%22%2C%5C%22objectType%5C%22%3A1%2C%5C%22nodeKey%5C%22%3A%5C%22MAINPAGE_SUBPAGE%5C%22%2C%5C%22page%5C%22%3A2%2C%5C%22objectCode%5C%22%3A%5C%22XNjAzOTM3ODY2MA%3D%3D%5C%22%2C%5C%22utdid%5C%22%3A%5C%22xEjfHP4g7hYCAdvkhxssZXp0%5C%22%2C%5C%22gray%5C%22%3A0%2C%5C%22tabCode%5C%22%3A101%7D%22%2C%22debug%22%3A0%7D'
 
 """
 UA伪装json模板
@@ -13,11 +15,44 @@ origin它指示请求的发起源,即发送请求的网页或者应用程序的�
 referer表示当前请求的上一个网页地址
 """
 
-def get_comment(url):
+def get_comment():
     """
     获取视频的评论
     """
-    pass
+    headers = {
+        "Accept": "application/json",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Connection": "keep-alive",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Cookie": "isI18n=false; cna=xEjfHP4g7hYCAdvkhxssZXp0; __ysuid=17040947675285Ra; youku_history_word=%5B%22%E6%97%A0%E9%97%B4%E9%81%93%22%5D; __ayft=1704158431897; __aysid=1704539138854hkh; __ayscnt=2; xlly_s=1; __arycid=dc-3-00; __arcms=dc-3-00; l=fBS_NpURPp3wZWupBO5ZKurza77tCLOf1sPzaNbMiIEGB6qsR3Q3JPKQVeYO3lT1WhQNesNwJ3S3hc2kBxYAZyIInxv9-Qy6XFHmndhyN3pR.; _m_h5_tk=e716162ff851dd1f47dccdfe4218aea4_1704727826654; _m_h5_tk_enc=3aa6f735a901cb95121fa1a0916c5197; __arpvid=1704724116959Z3jZIN-1704724116964; __aypstp=18; __ayspstp=16; __ayvstp=27; __aysvstp=27; tfstk=eczy7hsktaQPg4R_4m0E7StWnX3-BVB6-yMItWVnNYDoOWb3L5wJVDX-Pyy4_oL7d4T73wyTDpnKp8M0D7zwNYHL2w4YRjSoxyMQ86u8luTIFT3UtRgFCO_157F-k2X1CNCyrq0dP55KJNN8wIAyK1Zc5e5I7Tsb_ACHYUmOI5H0oOlGRi-kdxYH-zbK4A-Ipjh4uknrIm2w-maqa0kgZgzHpjbsWyEyKHooMjk1gskwYDVJOw0bIHKKmVhqC_GWvHnoGetz7FxpvmcrgA1WS; isg=BI-P9j-hNhprkjLo_fwgcvqXHiOZtOPWr_bvjqGbAf8icK1yqYfHJ_MmcqBOCLtO",
+        "Host": "acs.youku.com",
+        "Origin": "https://v.youku.com",
+        "Referer": "https://v.youku.com/v_show/id_XNTk3NzEyOTIyMA==.html?lang=%E8%8B%B1%E8%AF%AD&spm=a2hje.13141534.1_4.d_1_21&scm=20140719.apircmd.239009.video_XNTk3NzEyOTIyMA==",
+        "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    } # headers伪装ok
+
+    payload = {
+        "jsv": "2.6.1",
+        "appKey": "24679788",
+        "t": "1704724129394",
+        "sign": "5d680eaae12b7d35ee71456a3b1260d0",
+        "api": "mtop.youku.ycp.comment.detailpage.module.get",
+        "type": "originaljson",
+        "v": "1.0",
+        "ecode": "1",
+        "dataType": "json",
+        "data": '{"app":"100-DDwODVkv","time":1704724129393,"objectCode":"XNTk3NzEyOTIyMA==","objectType":1,"dataSource":"COMMENT_REPLY_LIST_DATASOURCE","page":1,"limit":2,"lastId":false,"bizParam":"{\"commentId\":1000907380228}","sign":"1cba82f56cf3300ed0218482dddd1dd2"}'
+    }
+
+    response = requests.get(url, headers=headers)
+    print(response.text)
 
 def get_conetent(url):
     headers= {
@@ -115,5 +150,6 @@ def get_and_write(path):
         print(f"写入{path}出现错误: ", e)
 
 if __name__ == "__main__":
-    print(get_conetent(one_url))
+    # print(get_conetent(one_url))
     # get_and_write("电影.xlsx")
+    get_comment()
